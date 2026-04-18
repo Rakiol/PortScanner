432fd6ed8132ea8c6704ea7e3a71d92769afcfb9
Rami Eter <ramieter.a@gmail.com>
2026-04-18 22:40:34 +0200

    Add initial port scanner implementation and learn sockets

---
+++ echo-client.py
@@ -0,0 +1,9 @@
+import socket
+
+HOST = ("127.0.0.1", 65432)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST))
+    s.sendall((b"Hello, World"))
+    data = s.recv(1024)
+print(f"Received {data!r}")
