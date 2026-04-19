11229f7f29e769f5f34ff512f2343be7a5f81332
Rami Eter <ramieter.a@gmail.com>
2026-04-20 00:14:31 +0200

    restructure port scanner with functions and file output
    - Add scan_port() for single port check with open/closed output
    - Add scan_range() to scan a port range and write open ports to openPorts.txt
    - Add user input flow with single port or range selection
    - Add if __name__ == "__main__" entry point

--- connect.py
+++ connect.py
@@ -1,15 +1,38 @@
 # use the learning for the scanner
 import socket
 
-#address = ("192.168.2.108", 0)
-
-for i in range(0, 100):
-    address = ("Scanme.nmap.org", i)
+def scan_port(host, port):
+    address = (host, port)
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mySocket.settimeout(0.5)
     if mySocket.connect_ex(address) == 0:
-        print(address[1], "port is open")
+        print("Port ", address[1], " is OPEN")
     else:
-        #pass
-        print(address[1], "Close")
+        print("PORT ", address[1], " is CLOSED")
+    mySocket.close()
 
+def scan_range(host, range0, range1):
+
+    for i in range(range0, range1):
+        mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mySocket.settimeout(0.5)
+        address = (host, i)
+        file = open("openPorts.txt", "a")
+        if mySocket.connect_ex(address) == 0:
+            file.write(str(i) + " is Open\n")
+        else:
+            pass
+        mySocket.close()
+
+if __name__ == "__main__":
+    a = input("Please write IP or Domain\n")
+    b = int(input("Please write 1 = Single Port or 2 = Range\n"))
+    if b == 1:
+        c = int(input("Write PORT number\n"))
+        scan_port(a, c)
+    else:
+        c = int(input("Write Start Range\n"))
+        d = int(input("Write End Range\n"))
+        scan_range(a, c, d)
+    print()
+    print("Finished")
