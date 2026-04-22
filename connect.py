3ef21735512b12034b55a1fa56584758efca2cb9
Rami Eter <ramieter.a@gmail.com>
2026-04-22 14:46:15 +0200

    feat: add sys.argv CLI, service name detection and error handling
    - Replace interactive input with sys.argv argument parsing
    - Add argument validation with usage hints
    - Add socket.getservbyport() to display service names for open ports
    - Handle unknown ports with try/except fallback to "unknown"
    - Apply service detection in both scan_port() and scan_range()

--- connect.py
+++ connect.py
@@ -1,26 +1,36 @@
 # use the learning for the scanner
 import socket
+import sys
 
-def scan_port(host, port):
-    address = (host, port)
+
+
+
+def scan_port(ip_scan, port):
+    address = (ip_scan, port)
     mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mySocket.settimeout(0.5)
     if mySocket.connect_ex(address) == 0:
-
-        print("Port ", address[1], " is OPEN and Service: ", socket.getservbyport(address[1]) )
+        try:
+            ser = socket.getservbyport(address[1])
+        except:
+            ser = "unknown"
+        print("Port ", address[1], " is OPEN and Service: ", ser )
     else:
         print("PORT ", address[1], " is CLOSED")
     mySocket.close()
 
-def scan_range(host, range0, range1):
+def scan_range(ip_scan, range_first, range_end):
     with open("openPorts.txt", "a") as f:
-        for i in range(range0, range1 + 1):
+        for i in range(range_first, range_end + 1):
             mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             mySocket.settimeout(0.5)
-            address = (host, i)
+            address = (ip_scan, i)
             print("Scanning Port: ", i)
             if mySocket.connect_ex(address) == 0:
-                n = socket.getservbyport(i)
+                try:
+                    n = socket.getservbyport(i)
+                except:
+                    n = "unknown"
                 f.write(str(i) + " is Open and Service: " + n + "\n")
             else:
                 pass
@@ -28,15 +38,22 @@ def scan_range(host, range0, range1):
 
 
 if __name__ == "__main__":
-    a = input("Please write IP or Domain\n")
-    b = int(input("Please write 1 = Single Port or 2 = Range\n"))
-    if b == 1:
-        c = int(input("Write PORT number\n"))
-        scan_port(a, c)
+
+    #print(len(sys.argv))
+    if len(sys.argv) < 3:
+        print("You need min. 2 Arguments")
+        sys.exit(1)
+    elif len(sys.argv) == 3:
+        ip_scan = sys.argv[1]
+        port = int(sys.argv[2])
+        scan_port(ip_scan, port)
+    elif len(sys.argv) == 4:
+        ip_scan = sys.argv[1]
+        range_first = int(sys.argv[2])
+        range_end = int(sys.argv[3])
+        scan_range(ip_scan, range_first, range_end)
     else:
-        c = int(input("Write Start Range\n"))
-        d = int(input("Write End Range\n"))
-        scan_range(a, c, d)
-    print()
-    print("Finished")
+        print("Wrong number of Arguments use help for help")
+
+
 
